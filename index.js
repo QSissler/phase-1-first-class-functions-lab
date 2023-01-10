d00110ab44ff16fa372a9a8242a6bfbade23fddf
Quinn Sissler <QSissler@gmail.com>
2023-01-10 13:21:00 -0500

    commit tree 9d3a757fcc0d4071b0a7e998015c646c50535f4d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (arrayOfNames){
+    return [arrayOfNames[0], arrayOfNames[1]];
+}
+
+const returnLastTwoDrivers = function (arrayOfNames){
+    return arrayOfNames.slice(-2);
+}
+const selectingDrivers = [
+returnFirstTwoDrivers, 
+returnLastTwoDrivers
+];
+
+function createFareMultiplier(int){
+    return function(fare){
+       return (int * fare);
+    }
+}
+
+console.log(createFareMultiplier(10)(2))
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(array, funct){
+    return funct(array);
+}
